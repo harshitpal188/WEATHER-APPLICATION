b4d628d3db9f5942993e0d4bd0638f1abacdbbd9
harshitpal188 <harshitpal188@gmail.com>
2025-08-06 22:45:39 +0530

    first react commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ed1271a7b11ccd725d9de702bd32d1d4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
